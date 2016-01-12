90afd1472c79ab3e558420c82c48f72a27fca405
ChrisKegel2901 <christian.kegelmann@hotmail.de>
2016-01-12 15:03:20 +0100

    test

--- iDProj/ViewController.swift
+++ iDProj/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var Username: UITextField!
+    @IBOutlet weak var UserName: UITextField!
+    
     @IBOutlet weak var Password: UITextField!
 
+   
+    @IBOutlet weak var ErrorText: UITextView!
+    var returnValue: Bool = false
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -28,8 +32,22 @@ class ViewController: UIViewController {
     
     
     @IBAction func Login(sender: AnyObject) {
-        let username = Username.text
+    
+        let username = UserName.text
         let password = Password.text
+        
+        ErrorText.text = ""
+        if username! == "Detlef" && password == "123" {
+          returnValue = true
+        }
+        
+        if returnValue
+        {
+            self.performSegueWithIdentifier("showTable", sender: self)
+        }
+        else{
+            ErrorText.text = "Der Benutzername oder das Passwort ist nicht korrekt. Bitte versuche es erneut."
+        }
         
         print(username)
         print(password)
